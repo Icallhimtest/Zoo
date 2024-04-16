100a7df5a4019e4bef77aa7ac859f76156411cb3
Denis Vermylen <dve@odoo.com>
2024-04-16 14:58:19 +0200

    [ADD] happy animals route

--- zoo/controllers/controllers.py
+++ zoo/controllers/controllers.py
@@ -12,6 +12,13 @@ class Zoo(http.Controller):
     def get_animal_status(self, name, **kw):
         return http.request.env['zoo.animal'].search_read([('name', '=', name)], ['status'], limit=1)[0]['status']
 
+    @http.route('/zoo/get_happy_status', auth='public')
+    def get_animal_status(self, **kw):
+        happy_animals = http.request.env['zoo.animal'].sudo()
+        for name in ['happy', 'joy', 'excited', 'enthousiastic', 'bli', 'bla', 'blu'] + ['erf%s' % i for i in range(10)]:
+            happy_animals |= happy_animals.search([('name', 'ilike', f'%{name}%')], limit=1)
+        return str(happy_animals and happy_animals[0].status)
+
     @http.route('/zoo/he_bit_me', auth='public')
     def he_bit_me(self, **kw):
         cr = http.request.env.cr
